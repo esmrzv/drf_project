3dfb3e7d13bf9ad0eecaa01d95085e33dba33c5e
esmrzv <esmurziev.1999@mail.ru>
2024-08-10 13:12:21 +0300

    Добалены новые модели

--- users/models.py
+++ users/models.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+from lms.models import Course, Lesson
+
 
 class User(AbstractUser):
     username = None
@@ -23,4 +25,15 @@ class User(AbstractUser):
         verbose_name_plural = 'Пользователи'
 
 
+class Payment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    payment_date = models.DateTimeField(auto_now_add=True, verbose_name='Дата оплаты')
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    lesson = models.ForeignKey(Lesson, null=True, blank=True,
+                               on_delete=models.CASCADE)  # Урок может быть не обязательно
+    amount = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Сумма оплаты')
+    payment_method = models.CharField(max_length=10, choices=[('cash', 'Наличные'), ('transfer', 'Перевод на счет')],
+                                      default='transfer', verbose_name='способ оплаты')
 
+    def __str__(self):
+        return f'Оплатил: {self.user} по {self.course} в {self.payment_date}'
