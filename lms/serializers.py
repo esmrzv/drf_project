7d94a990f6e12345381550c8ee3df930b0766f91
esmrzv <esmurziev.1999@mail.ru>
2024-08-10 13:03:12 +0300

    Добавлено колличество уроков в сериализатор

--- lms/serializers.py
+++ lms/serializers.py
@@ -4,9 +4,14 @@ from lms.models import Course, Lesson
 
 
 class CourseSerializer(serializers.ModelSerializer):
+    lesson_count = serializers.SerializerMethodField()
+
     class Meta:
         model = Course
         fields = '__all__'
+
+    def get_lesson_count(self, obj):
+        return Lesson.objects.filter(course=obj).count()
 
 
 class LessonSerializer(serializers.ModelSerializer):
