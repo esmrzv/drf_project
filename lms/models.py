22171985055ac96ac1d812cee5480105c1a5cf71
esmrzv <esmurziev.1999@mail.ru>
2024-08-19 01:09:45 +0300

    добавлены фикстуры и поле для владельца

--- lms/models.py
+++ lms/models.py
@@ -1,10 +1,14 @@
 from django.db import models
 
+from config import settings
+
 
 class Course(models.Model):
     name = models.CharField(max_length=100, verbose_name='Название')
     preview = models.ImageField(upload_to='media/course', verbose_name='Превью', null=True, blank=True)
     description = models.TextField(verbose_name='Описание')
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, blank=True,
+                              verbose_name='владелец')
 
     def __str__(self):
         return f'{self.name} - {self.description}'
@@ -20,6 +24,8 @@ class Lesson(models.Model):
     description = models.TextField(verbose_name='Описание')
     course = models.ForeignKey(Course, on_delete=models.CASCADE, verbose_name='Курс')
     video_link = models.URLField(verbose_name='Ссылка на видео', null=True, blank=True)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, blank=True,
+                              verbose_name='владелец')
 
     def __str__(self):
         return f'{self.name} - {self.description}'
--- users/permissions.py
+++ users/permissions.py
@@ -4,3 +4,11 @@ from rest_framework import permissions
 class IsModer(permissions.BasePermission):
     def has_permission(self, request, view):
         return request.user.groups.filter(name='moders')
+
+
+class IsOwner(permissions.BasePermission):
+
+    def has_object_permission(self, request, view, obj):
+        if obj.owner == request.user:
+            return True
+        return False
