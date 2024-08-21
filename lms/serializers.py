4af94ca605d682b525344958bfeb7f922114248e
esmrzv <esmurziev.1999@mail.ru>
2024-08-21 12:19:43 +0300

    реализован валидатор для фильтрации ссылок

--- lms/serializers.py
+++ lms/serializers.py
@@ -1,12 +1,18 @@
 from rest_framework import serializers
 
 from lms.models import Course, Lesson
+from lms.validators import LessonCustomValidator
 
 
 class LessonSerializer(serializers.ModelSerializer):
     class Meta:
         model = Lesson
         fields = '__all__'
+
+    def validate_video_link(self, value):
+        validator = LessonCustomValidator(field='video_link')
+        validator(value)
+        return value
 
 
 class CourseSerializer(serializers.ModelSerializer):
