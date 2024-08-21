12fb7b944832fafeb387c57aa989ce99aa45614b
esmrzv <esmurziev.1999@mail.ru>
2024-08-21 15:20:23 +0300

    реализована подписка для курса

--- lms/serializers.py
+++ lms/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from lms.models import Course, Lesson
+from lms.models import Course, Lesson, Subscription
 from lms.validators import LessonCustomValidator
 
 
@@ -18,10 +18,23 @@ class LessonSerializer(serializers.ModelSerializer):
 class CourseSerializer(serializers.ModelSerializer):
     lesson_count = serializers.SerializerMethodField()
     lessons = LessonSerializer(many=True, read_only=True, source='lesson_set')
+    subscribers = serializers.SerializerMethodField()
 
     class Meta:
         model = Course
-        fields = ('id', 'name', 'description', 'lesson_count', 'lessons')
+        fields = ('id', 'name', 'description', 'lesson_count', 'lessons', 'subscribers')
 
     def get_lesson_count(self, obj):
         return Lesson.objects.filter(course=obj).count()
+
+    def get_is_subscribed(self, obj):
+        request = self.context.get('request')
+        if request and request.user.is_authenticated:
+            return Subscription.objects.filter(user=request.user, course=obj).exists()
+        return False
+
+
+class SubscriptionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Subscription
+        fields = '__all__'
