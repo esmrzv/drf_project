2e447e07d175100670660cbb4c6dce8289f3aaae
esmrzv <esmurziev.1999@mail.ru>
2024-08-30 18:25:51 +0300

    добавлен Celery в проект

---
+++ lms/tasks.py
@@ -0,0 +1,19 @@
+from celery import shared_task
+from config.settings import EMAIL_HOST_USER
+from django.core.mail import send_mail
+
+
+@shared_task
+def check_subscribe(email):
+    try:
+        send_mail(
+            subject='Обновление курса',
+            message=f'Курс по подписке обновлен',
+            from_email=EMAIL_HOST_USER,
+            recipient_list=[email]
+
+        )
+    except Exception as e:
+        print(f'Ошибка при отправке письма {e}')
+
+
